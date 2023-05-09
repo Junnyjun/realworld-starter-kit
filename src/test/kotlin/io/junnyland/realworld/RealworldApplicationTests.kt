969130d58d764083852ed74ed54d0f6fda3dc283
junnyJun <chbe5082@naver.com>
2023-05-09 17:47:36 +0900

    init

---
+++ src/test/kotlin/io/junnyland/realworld/RealworldApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.junnyland.realworld
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RealworldApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
