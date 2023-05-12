209234ac26c4386902156bd9c0f234de0528b852
JunnyJun <chbe5082@naver.com>
2023-05-12 17:52:39 +0900

    removed old version

--- src/main/kotlin/io/junnyland/realworld/user/action/out/security/SecurityProperties.kt
+++
@@ -1,8 +0,0 @@
-package io.junnyland.realworld.user.action.out.security
-
-
-object SecurityProperties {
-    const val SECRETS = "realworld"
-    const val ISSUEER = "junnyland"
-    const val EXPIRED = 2000L
-}
